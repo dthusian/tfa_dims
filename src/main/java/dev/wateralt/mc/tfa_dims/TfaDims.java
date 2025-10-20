ea49bf2db18217c303d7c8810bbc1ccf5e9ca756
Adrian Wu <eattofueveryday@gmail.com>
2025-10-19 23:21:02 -0400

    Initial commit

---
+++ src/main/java/dev/wateralt/mc/tfa_dims/TfaDims.java
@@ -0,0 +1,10 @@
+package dev.wateralt.mc.tfa_dims;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TfaDims implements ModInitializer {
+
+  @Override
+  public void onInitialize() {
+  }
+}
